2b074527d556c52dc7257ca8f1b0466713d1abc7
soufianetaf <itssoufianetafahi@gmail.com>
2026-06-02 19:34:50 +0100

    Feat : mise a jour de la connexion a la base de donnees

--- src/utils/db_connection.py
+++ src/utils/db_connection.py
@@ -1,11 +1,26 @@
-
 import psycopg2
+from dotenv import load_dotenv, find_dotenv
+import os
+load_dotenv(find_dotenv())
 
-def conn():
-    con = psycopg2.connect(
-        dbname="olist_ecom",
-        user = "postgres",
-        password = "0000",
-        host = "localhost"
-    )
-    return con
+def get_connexion():
+    """
+    creer connexion avec notre base de donnees
+    Sorties :
+    conn : retourner la connexion a la base de données
+    """
+
+    try :
+        conn = psycopg2.connect(
+            host=os.getenv("db_host"),
+            database = os.getenv("db_name"),
+            user= os.getenv("dv_user"),
+            password = os.getenv("db_password")
+        )
+
+        print("Connexion a la base de données est reussie.")
+        return conn
+    except Exception as e:
+        print("Erreur a la connexion a la base de données")
+        print(f"Details de l'erreur: {e}")
+        return None
