028f0aab849197b363f774510afd0df397044981
soufianetaf <itssoufianetafahi@gmail.com>
2026-06-03 20:11:28 +0100

    Feat : manipuler les donnes manquantes et duplicantes

--- sql/03_transform_staging.sql
+++ sql/03_transform_staging.sql
@@ -64,3 +64,55 @@ CREATE TABLE silver.olist_order_items (
        FOREIGN KEY(order_id)
        REFERENCES silver.olist_orders(order_id)
 );
+
+
+
+-- 5. Nettoyage et insertion des Clients
+INSERT INTO silver.olist_customers (customer_id, customer_unique_id, customer_zip_code_prefix, customer_city, customer_state)
+SELECT 
+    customer_id,
+    customer_unique_id,
+    CAST(customer_zip_code_prefix AS INT),
+    customer_city,
+    customer_state
+FROM raw.olist_customers;
+
+-- 6. Nettoyage et insertion des Produits (Gestion des NULLs !)
+INSERT INTO silver.olist_products 
+SELECT 
+    product_id,
+    COALESCE(product_category_name, 'Unknown'),                   -- Remplace NULL par 'Unknown'
+    CAST(COALESCE(product_name_lenght, '0') AS INT),              -- Remplace NULL par 0
+    CAST(COALESCE(product_description_lenght, '0') AS INT),
+    CAST(COALESCE(product_photos_qty, '0') AS INT),
+    CAST(COALESCE(product_weight_g, '0') AS INT),
+    CAST(COALESCE(product_length_cm, '0') AS INT),
+    CAST(COALESCE(product_height_cm, '0') AS INT),
+    CAST(COALESCE(product_width_cm, '0') AS INT)
+FROM raw.olist_products;
+
+-- 7. Nettoyage et insertion des Commandes (Gestion des Dates !)
+INSERT INTO silver.olist_orders 
+SELECT 
+    order_id,
+    customer_id,
+    order_status,
+    -- NULLIF('' , '') transforme une chaîne de texte vide de notre CSV en un vrai NULL SQL
+    CAST(NULLIF(order_purchase_timestamp, '') AS TIMESTAMP),
+    CAST(NULLIF(order_approved_at, '') AS TIMESTAMP),
+    CAST(NULLIF(order_delivered_carrier_date, '') AS TIMESTAMP),
+    CAST(NULLIF(order_delivered_customer_date, '') AS TIMESTAMP),
+    CAST(NULLIF(order_estimated_delivery_date, '') AS TIMESTAMP)
+FROM raw.olist_orders;
+
+-- 8. Nettoyage et insertion des Articles commandés
+INSERT INTO silver.olist_order_items 
+SELECT 
+    order_id,
+    CAST(order_item_id AS INT),
+    product_id,
+    seller_id,
+    CAST(NULLIF(shipping_limit_date, '') AS TIMESTAMP),
+    CAST(price AS NUMERIC(10,2)),
+    CAST(freight_value AS NUMERIC(10,2))
+FROM raw.olist_order_items;
